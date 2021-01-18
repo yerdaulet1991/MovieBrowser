76e3aa89e4d53a8b86eae92f89d1c31395aeedc0
yerdaulet1991 <yalibayev@nu.edu.kz>
2021-01-18 15:27:22 +0600

    finished app

--- app/src/main/java/com/yerdaulet/moviebrowser/BaseApplication.kt
+++ app/src/main/java/com/yerdaulet/moviebrowser/BaseApplication.kt
@@ -1,4 +1,91 @@
 package com.yerdaulet.moviebrowser
 
-class BaseApplication {
+import android.app.Application
+import androidx.room.Room
+import com.yerdaulet.moviebrowser.api.MoviesApi
+import com.yerdaulet.moviebrowser.models.Movie
+import com.yerdaulet.moviebrowser.persistence.MovieDB
+import com.yerdaulet.moviebrowser.repository.MovieDetailRepository
+import com.yerdaulet.moviebrowser.repository.MoviesRepository
+import com.yerdaulet.moviebrowser.ui.detail.fragments.DetailsFragmentViewModel
+import com.yerdaulet.moviebrowser.ui.list.MovieListViewModel
+import com.yerdaulet.moviebrowser.util.AppExecutors
+import com.yerdaulet.moviebrowser.util.BASE_URL
+import com.yerdaulet.moviebrowser.util.DATABASE_NAME
+import com.yerdaulet.moviebrowser.util.LiveDataCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class BaseApplication : Application() {
+    private val appModule = module {
+        single<MoviesApi> {
+            val logger = HttpLoggingInterceptor()
+            logger.setLevel(HttpLoggingInterceptor.Level.BASIC)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .connectTimeout(60L, TimeUnit.SECONDS)
+                .readTimeout(60L, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(false)
+                .build()
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(LiveDataCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build().create(MoviesApi::class.java)
+        }
+        viewModel<MovieListViewModel> {
+            val repository: MoviesRepository = get()
+            MovieListViewModel(
+                repository, this@BaseApplication
+            )
+        }
+        single<MovieDB> {
+            Room.databaseBuilder(
+                this@BaseApplication.applicationContext, MovieDB::class.java,
+                DATABASE_NAME
+            ).build()
+        }
+
+        single<MoviesRepository> {
+            val movieDB: MovieDB = get()
+            val appExecutors: AppExecutors = get()
+            val moviesApi: MoviesApi = get()
+            MoviesRepository(movieDB.movieDao, appExecutors, moviesApi)
+        }
+
+        single<MovieDetailRepository> {
+            val moviesApi: MoviesApi = get()
+            MovieDetailRepository(moviesApi)
+        }
+
+        single<AppExecutors> {
+            AppExecutors()
+        }
+
+        viewModel<DetailsFragmentViewModel> { (movie: Movie)->
+            val repository: MovieDetailRepository = get()
+            DetailsFragmentViewModel(
+                this@BaseApplication,
+                repository, movie
+            )
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@BaseApplication)
+            modules(appModule)
+        }
+    }
 }
